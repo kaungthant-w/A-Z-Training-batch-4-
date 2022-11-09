2f7b8513ee4953f50e7442ac039242641b47ddaa
“kaungthant-w” <kaungthant9513@gmail.com>
2022-11-09 12:47:39 +0630

    05_Arithmetic_Operators_&_Comparison_Operators

---
+++ JS/05_Arithmetic_Operators_&_Comparison_Operators.js
@@ -0,0 +1,55 @@
+// let else = "kaung thant";
+// console.log(else);
+
+// let function = "Kyaw Thant";
+// console.log(function);
+
+let num1 =20;
+let num2 = 10;
+let num3 = "20";
+
+
+//arithmetic operators
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+
+// num1++
+console.log(num1++);
+console.log(num1);
+
+//num++
+console.log(++num1);
+console.log(num1);
+
+
+// num1--
+console.log(num1--);
+console.log(num1);
+
+//num1--
+console.log(--num1);
+console.log(num1);
+
+
+//comparison operators
+
+//  ==, != value only
+// === , !== value + type 
+console.log(num1 == num2);
+console.log(num1 != num2);
+
+
+console.log(num1 === num2);
+console.log(num2 === num3);
+
+console.log(num2 !== num3);
+
+console.log(num1 > num2);
+console.log(num1 >= num2);
+console.log(num1 < num2);
+console.log( num1 <= num2);
+console.log();
+console.log();
