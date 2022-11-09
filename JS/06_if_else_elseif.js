5f83f9ac61d8fbf1b4a1ad996386aa4cae7f5394
“kaungthant-w” <kaungthant9513@gmail.com>
2022-11-09 13:14:52 +0630

    If...else

---
+++ JS/06_if_else_elseif.js
@@ -0,0 +1,37 @@
+console.log("if...else");
+
+let fruit1 = "apple";
+if(fruit1 == "apple") {
+    console.log("This is Apple");
+}
+
+console.log("-------The end----------");
+
+let fruit2 = "orange";
+if(fruit2 == "apple") {
+    console.log("This is Orange");
+}
+
+console.log("-------The end----------");
+
+let fruit3 = "tomato";
+if(fruit3 == "apple") {
+    console.log("This is apple");
+} else {
+    console.log("This is another fruit");
+}
+
+console.log("-------The end----------");
+
+let fruit4 = "watermelon";
+if(fruit4 == "apple") {
+    console.log("This is apple");
+} else if (fruit4 == "orange"){
+    console.log("This is orange");
+} else if(fruit4 == "tomato") {
+    console.log("This is orange");
+} else {
+    console.log("This is another fruit");
+}
+
+console.log("-------The end----------");
